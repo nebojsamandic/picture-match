c7cae30eb50f06f621b0a079a7177b5abbe4a83e
Nebojsa Mandic <nebojsa.mandic18@spsmail.cuny.edu>
2025-03-31 12:29:06 -0400

    newsest audio update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 const audio = document.getElementById('puzzleAudio');
 
+document.body.addEventListener('mouseover', () => {
+    audio.play().catch(error => {
+        console.error('Error playing audio:', error);
+    });
+});
+
 audio.volume = 0.3;
