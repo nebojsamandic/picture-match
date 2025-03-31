aabad536558489a213a5d08c6aa36a91dd84ebe4
Nebojsa Mandic <nebojsa.mandic18@spsmail.cuny.edu>
2025-03-31 15:50:16 -0400

    newsest audio update

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@ document.body.addEventListener('mouseover', () => {
 });
 
 audio.volume = 0.3;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const startButton = document.querySelector(".puzzle__start_button");
+    const startScreen = document.querySelector(".puzzle__start");
+    const heatMirage = document.querySelector(".heat-mirage");
+    const puzzleGrid = document.querySelector(".puzzle__grid");
+
+    startButton.addEventListener("click", () => {
+        startScreen.classList.add("fade-out");
+        heatMirage.classList.add("fade-out");
+
+        setTimeout(() => {
+            puzzleGrid.classList.add("fade-in");  
+        }, 1000);
+    });
+});
